b8b2dda4739a37d2f47be884c0153fd3cb6459d8
Ivan Caminal <14839609+Ivancaminal72@users.noreply.github.com>
2023-05-28 19:02:44 +0200

    first version of the server

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/links", async (req, res) => {
+  try {
+    const result = await prisma.link.findMany({});
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({ type: e.constructor.name, message: e.toString() });
+  }
+});
+
+const { SERVER_PORT } = process.env;
+app.listen(SERVER_PORT, () => {
+  console.log(`Links API listening on :${SERVER_PORT}`);
+});
